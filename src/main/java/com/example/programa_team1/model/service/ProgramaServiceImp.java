f90850cc8aab5f4a514baaed4b5280edc1fc9df3
Juan David Cobo Aguirre <juan.cobo01@uceva.edu.co>
2024-04-18 17:13:33 -0500

    Para ProgramaServiceImp se añadio el @Autowired

--- src/main/java/com/example/programa_team1/model/service/ProgramaServiceImp.java
+++ src/main/java/com/example/programa_team1/model/service/ProgramaServiceImp.java
@@ -2,6 +2,7 @@ package com.example.programa_team1.model.service;
 
 import com.example.programa_team1.model.dao.IProgramaDao;
 import com.example.programa_team1.model.entities.Programa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,10 +12,15 @@ public class ProgramaServiceImp implements IProgramaService {
 
     IProgramaDao programaDao;
 
+    @Autowired
+    public ProgramaServiceImp(IProgramaDao programaDao){this.programaDao = programaDao;}
+
     @Override
     public List<Programa> findAll(){
         return (List<Programa>)programaDao.findAll();
     }
+
+
     /*
     Cuando estén todos los metodos quitar el comentario de arriba para que reconozca
     las implementaciones hechas en IProgramaService
