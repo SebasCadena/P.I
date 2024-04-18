906d9bb80f36a7595b7ea7044eac411e13f7f409
Juan David Cobo Aguirre <juan.cobo01@uceva.edu.co>
2024-04-18 17:12:02 -0500

    En ProgramaRestController se añadio @GetMapping para listar los programas

--- src/main/java/com/example/programa_team1/controller/ProgramaRestController.java
+++ src/main/java/com/example/programa_team1/controller/ProgramaRestController.java
@@ -1,18 +1,26 @@
 package com.example.programa_team1.controller;
 
+import com.example.programa_team1.model.entities.Programa;
 import com.example.programa_team1.model.service.IProgramaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/programa")
 public class ProgramaRestController {
 
     private IProgramaService programaService;
-    
+
     @Autowired
     public ProgramaRestController(IProgramaService programaService){
         this.programaService = programaService;
     }
+    @GetMapping("/programas")
+    public List<Programa> listar() {
+        return this.programaService.findAll();
+    }
+
 
 }
