57f74c25ed97be713d028200854a04f5d35ed182
Sebas Cadena <juanseca020405@gmail.com>
2024-04-18 08:05:57 -0500

    Creación Packages controller y model Programa(dao, entities, service)

---
+++ src/main/java/com/example/programa_team1/model/entities/Programa.java
@@ -0,0 +1,20 @@
+package com.example.programa_team1.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "programa")
+public class Programa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int codPrograma, codFacultad;
+    private String nomPrograma, nomFacultad;
+
+
+}
