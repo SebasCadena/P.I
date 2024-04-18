c52a133464a83923f1a0c2b309b0e517f5ba2d46
Sebas Cadena <juanseca020405@gmail.com>
2024-04-17 21:05:32 -0500

    Commit inicial entidad programa

---
+++ src/main/java/com/example/programa_team1/ProgramaTeam1Application.java
@@ -0,0 +1,13 @@
+package com.example.programa_team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaTeam1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramaTeam1Application.class, args);
+    }
+
+}
