2e97ebddd1e1245ae959fc456d22ea25bc23fec6
Juan David Cobo Aguirre <juan.cobo01@uceva.edu.co>
2024-04-18 17:10:12 -0500

    Se añadieron mas registros para la tabla import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
-INSERT INTO programa (codPrograma, codFacultad, nomPrograma, nomFacultad) VALUES ('10587','1','Ingenieria en sistemas', 'Ingenieria');
+INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria de sistemas', 'Ingenieria');
+INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria biomedica', 'Ingenieria');
+INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria electronica', 'Ingenieria');
+INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria ambiental', 'Ingenieria');
+INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria industrial', 'Ingenieria');
