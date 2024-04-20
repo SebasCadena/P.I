10384cc3da107f56ed6aeb6d2341dffd5052a4af
Juan David Cobo Aguirre <juan.cobo01@uceva.edu.co>
2024-04-20 17:23:18 -0500

    Se cambio una sentencia del import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,4 @@ INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria de sistemas'
 INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria biomedica', 'Ingenieria');
 INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria electronica', 'Ingenieria');
 INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria ambiental', 'Ingenieria');
+INSERT INTO programa (nomPrograma, nomFacultad) VALUES ('Ingenieria agropecuaria', 'Ingenieria');
