9e4489e0ac57af9064c6a5c792c9989dad717a09
Juan David Cobo Aguirre <juan.cobo01@uceva.edu.co>
2024-05-21 13:53:44 -0500

    (Refactor)e cambiaron las urls de los endpoints para el funcionamiento del crud de la api programa.

--- src/main/java/com/example/programa_team1/controller/ProgramaRestController.java
+++ src/main/java/com/example/programa_team1/controller/ProgramaRestController.java
@@ -2,13 +2,16 @@ package com.example.programa_team1.controller;
 
 import com.example.programa_team1.model.entities.Programa;
 import com.example.programa_team1.model.service.IProgramaService;
+import com.example.programa_team1.configuracion.ResourseNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/programa")
+@RequestMapping("/api/programa-service")
 public class ProgramaRestController {
 
     private IProgramaService programaService;
@@ -17,27 +20,52 @@ public class ProgramaRestController {
     public ProgramaRestController(IProgramaService programaService){
         this.programaService = programaService;
     }
+
+
+    //Este metodo obtiene el listado de los programas
     @GetMapping("/programas")
     public List<Programa> listar() {
         return this.programaService.findAll();
     }
 
-    @PostMapping("/create")
+    //Este metodo añade un programa a la lista
+    @PostMapping("/programa")
     public Programa createPrograma(@RequestBody Programa programa){
         return this.programaService.save(programa);
     }
 
-    @PutMapping("/update")
-    public Programa updatePrograma(@RequestBody Programa programa){
-        return this.programaService.update(programa);
+    //Este metodo obtiene un programa por su id
+    @GetMapping("/programa/{codprograma}")
+    public ResponseEntity<Programa> buscarPrograma(@PathVariable Long codprograma) {
+        Optional<Programa> optionalPrograma = Optional.ofNullable(programaService.findById(codprograma));
+        if (optionalPrograma.isPresent()) {
+            Programa programa = optionalPrograma.get();
+            return ResponseEntity.ok(programa);
+        } else {
+            throw new ResourseNotFoundException("No existe el programa con el ID: " + codprograma);
+        }
     }
 
-    @GetMapping("/programas/{codprograma}")
-    public Programa buscarPrograma(@PathVariable Long codprograma){
-        return this.programaService.findById(codprograma);
+    //Metodo que actualiza un programa
+    @PutMapping("/programa/{codprograma}")
+    public ResponseEntity<Programa> actualizarPrograma(@PathVariable Long codprograma, @RequestBody Programa detallesPrograma) {
+        Optional<Programa> optionalPrograma = Optional.ofNullable(programaService.findById(codprograma));
+        if (optionalPrograma.isPresent()) {
+            Programa programa = optionalPrograma.get();
+            programa.setNomfacultad(detallesPrograma.getNomfacultad());
+            programa.setNomprograma(detallesPrograma.getNomprograma());
+
+            Programa programaActualizado = programaService.save(programa);
+            return ResponseEntity.ok(programaActualizado);
+        } else {
+            throw new ResourseNotFoundException("No existe el programa con el ID: " + codprograma);
+        }
     }
 
-    @DeleteMapping("/delete/{codPrograma}")
+
+
+    //Este metodo borra un programa por su id
+    @DeleteMapping("/programa/{codPrograma}")
     public void deleteById(@PathVariable Long codPrograma){
         this.programaService.deleteById(codPrograma);
     }
